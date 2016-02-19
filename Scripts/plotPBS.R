8e1e2d05bfc9c885c04deec09d5ba3b3972e05fe
mfumagalli <oceanodigomma@gmail.com>
2016-02-19 13:32:37 +0000

    add refgebn

--- Scripts/plotPBS.R
+++ Scripts/plotPBS.R
@@ -14,7 +14,12 @@ par(mar=c(0, 4, 4, 2) + 0.1)
 
 pbs <- read.table(fin, header=TRUE)
 
-plot(pbs$midPos/1e6, pbs$PBS2, type ="l", col="#d95f02",ylab = "PBS", xlab=paste("Chromosome",pbs$chr[1]), main="PBS scan in PEL" , lwd=1, xaxt="n",cex.main = 0.9, cex.axis = 0.6, cex.lab = 0.68 ) #orange
+plot(pbs$midPos/1e6, pbs$PBS2, type ="l", col="orange",ylab = "PBS", xlab=paste("Chromosome",pbs$chr[1]), main="PBS scan" , lwd=1, xaxt="n",cex.main = 0.9, cex.axis = 0.6, cex.lab = 0.68 ) #orange
+
+lines(pbs$midPos/1e6, pbs$PBS1, col="blue", lwd=1)
+
+legend("topright", legen=c("PEL","CHB"), col=c("orange", "blue"), lty=1)
+
     #title(ylab="Selection Statistics", line=2.2, cex.lab=0.69)
     #abline(h=1,lty=2,col="grey",lwd=1)
 
